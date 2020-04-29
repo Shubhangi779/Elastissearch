5f4310f95579e1bece134ac2d00f8c0d9e987e34
Shubhangi779 <62985279+Shubhangi779@users.noreply.github.com>
2020-04-29 19:14:55 +0530

    Update SpringBootMain.java

--- src/main/java/com/infy/SpringBootMain.java
+++ src/main/java/com/infy/SpringBootMain.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages="com.infy.*")
+//@ComponentScan(basePackages="com.infy.*")
+@EnableElasticsearchRepositories(basePackages="com.infy.search")
+@EnableJpaRepositories(basePackages ="com.infy.repository")
 public class SpringBootMain {
 	public static void main(String[] args) {
         SpringApplication.run(SpringBootMain.class, args);
